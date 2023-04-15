87c781d27f11373c2fedd223799776589f543f13
Mohamed atef <mohammed.atef.saif@gmail.com>
2023-04-16 01:19:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let resultsContainer = document.getElementsByClassName("container")[0];
+let searchInput = document.getElementById("search-input");
+
+const debounce = (fn, delay) => {
+    let timeoutId;
+    return function (...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+        fn.apply(this, args);
+        }, delay);
+    };
+};
+
+const validateInput = (el) => {
+    if (el.value === "") {
+        resultsContainer.innerHTML = "<p>Type something in the above search input</p>";
+    } else {
+        generateResults(el.value, el);
+    }
+};
+
+const generateResults = (searchValue, inputField) => {
+    fetch(
+        "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=" +
+        searchValue
+    )
+        .then((response) => response.json())
+        .then((data) => {
+        let results = data.query.search;
+        let numberOfResults = data.query.search.length;
+        resultsContainer.innerHTML = "";
+        for (let i = 0; i < numberOfResults; i++) {
+            let result = document.createElement("div");
+            result.classList.add("results");
+            result.innerHTML = `
+            <div>
+                <h3>${results[i].title}</h3>
+                <p>${results[i].snippet}</p>
+            </div>
+            <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
+            `;
+            resultsContainer.appendChild(result);
+        }
+        if (inputField.value === "") {
+            resultsContainer.innerHTML =
+            "<p>Type something in the above search input</p>";
+        }
+        });
+    };
+
+    searchInput.addEventListener(
+    "input",
+    debounce(() => validateInput(searchInput), 500)
+);
